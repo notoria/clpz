c7032677595740556dbcc5a540bd816caa40c3a6
notoria <notoria@users.noreply.github.com>
2024-01-07 20:19:40 +0100

    Fix bound computation in `(^)/2`
    ?- A in 0..1, B in-1..0, A^2 #= B.
       false. % unexpected.
    ?- A^2 #= B, A in 0..1, B in-1..0.
       A = 0, B = 0.

--- clpz.pl
+++ clpz.pl
@@ -5495,7 +5495,7 @@ run_propagator(pexp(X,Y,Z), MState) -->
                 fd_put(Z, ZD2, ZPs),
                 { (   even(Y), ZU = n(Num) ->
                     integer_kth_root_leq(Num, Y, RU),
-                    (   XL cis_geq n(0), ZL = n(Num1) ->
+                    (   XL cis_geq n(0), ZL = n(Num1), Num1 >= 0 ->
                         integer_kth_root_leq(Num1, Y, RL0),
                         (   RL0^Y < Num1 -> RL is RL0 + 1
                         ;   RL = RL0
